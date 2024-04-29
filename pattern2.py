68573d390dea4550bdca704da306e88f5ec4ae31
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2024-04-29 15:52:52 +0530

    third pattern succesfully run

--- pattern2.py
+++ pattern2.py
@@ -2,13 +2,18 @@ n = 6
 x = 0
 for i in range(n*2):
     flag = False
+
+    #part 1
     if i<(n+1):
         print("@",end='')
     else:
         print(" ",end='')
-
+    
+    #part 2
     for j in range(n*(n-1)+1):
         if (i>=n//2) & (i<(2*n)-(n//2)):
+
+            #part 3
             if (j<(n-1)*x) | (j>(n-1)*x+n-1):
                 print(" ",end='')
             else:
@@ -20,6 +25,7 @@ for i in range(n*2):
     if flag:
         x = x+1
 
+    #part4
     if i>(n-1):
         print("@",end='')
     else:
---
+++ pattern3.py
@@ -0,0 +1,13 @@
+n = 9
+for i in range(n):
+    for j in range(n):
+        if ((i<n//2) & (j<=i))  |  ((i>n//2) & (j<n-i))  |  (i==n//2):
+            print("*",end='')
+        else:
+            print(" ",end='')
+
+    if (i==n//2-1) | (i==n//2+1):
+        print(" *",end='')
+    if i==n//2:
+        print("***",end='')
+    print()
